48172dd7b8d7d65d565ee7e8fc91f6c7df723510
edwilson <edwilsonfa@gmail.com>
2026-01-14 17:24:51 -0300

    Entendendo as Annotations [ApiController], [Route] e [FromQuery] no ASP.NET

---
+++ RestWithASPNET10Ed/RestWithASPNET10Ed/Controllers/MathController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET10Ed.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : ControllerBase {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber) {
+            if isNumeric(firstNumber) && isNumeric(secondNumber){
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum);
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string firstNumber) {
+            throw new NotImplementedException();
+        }
+
+        private bool isNumeric(string firstNumber) {
+            throw new NotImplementedException();
+        }
+    }
+}

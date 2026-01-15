c867a78f31e8017cd858a98057ddbd6161f56336
Ed Wilson <edwilsonfa@gmail.com>
2026-01-15 16:59:34 -0300

    Implementando IsNumeric e ConvertToDecimal

--- RestWithASPNET10Ed/RestWithASPNET10Ed/Controllers/MathController.cs
+++ RestWithASPNET10Ed/RestWithASPNET10Ed/Controllers/MathController.cs
@@ -6,19 +6,38 @@ namespace RestWithASPNET10Ed.Controllers {
     public class MathController : ControllerBase {
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Get(string firstNumber, string secondNumber) {
-            if isNumeric(firstNumber) && isNumeric(secondNumber){
+            if (isNumeric(firstNumber) && isNumeric(secondNumber)){
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum);
             }
             return BadRequest("Invalid input");
         }
 
-        private decimal ConvertToDecimal(string firstNumber) {
-            throw new NotImplementedException();
+
+        private bool isNumeric(string strNumber)
+        {
+            decimal decimalValue;
+            bool isNumber = decimal.TryParse(
+                strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue
+                );//BR 10,5 - US 10.5
+            return isNumber;
         }
 
-        private bool isNumeric(string firstNumber) {
-            throw new NotImplementedException();
+        private decimal ConvertToDecimal(string strNumber) {
+            decimal decimalValue;
+            if(decimal.TryParse(
+                strNumber,
+               System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimalValue)){
+                return decimalValue;
+            }
+            return 0;
         }
+
+
     }
 }
